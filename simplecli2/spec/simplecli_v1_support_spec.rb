a9bc1194434f05b1b2c652dfe0098faa36871d6e
remi <remi@remitaylor.com>
2009-04-03 14:53:17 -0700

    placeholder for V1 compatibility specs

---
+++ simplecli2/spec/simplecli_v1_support_spec.rb
@@ -0,0 +1,12 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+#
+# This should run abunchof my existing scripts that use SimpleCLI
+# and make sure that SimpleCLI2 is compatible with them so
+# everything will "just work" when we make SimpleCLI => SimpleCLI2
+#
+describe 'SimpleCLI version 1 support' do
+
+  # 
+
+end
