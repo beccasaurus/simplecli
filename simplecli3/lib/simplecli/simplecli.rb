e1741ec5d7e400fba4dbf07b88e14da60b777319
remi <remi@remitaylor.com>
2009-04-09 17:37:54 -0700

    got basic #command_names working with dynamic prefix/suffixes

--- simplecli3/lib/simplecli/simplecli.rb
+++ simplecli3/lib/simplecli/simplecli.rb
@@ -3,6 +3,20 @@
 #
 module SimpleCLI
 
+  # instance methods on the SimpleCLI module, callable as SimpleCLI.method
+  class << self
+    
+    # The prefix/suffix used for discovering commands.
+    #
+    # Default: '_help' suffix, nil prefix
+    #
+    # :api: public
+    attr_accessor :help_prefix, :help_suffix
+
+    SimpleCLI.help_suffix ||= '_help'
+
+  end
+
   # Get the names of all of the availabe commands
   #
   # ==== Returns
@@ -10,9 +24,13 @@ module SimpleCLI
   #
   # api: public
   def command_names
-    help_methods    = self.methods.grep /_help$/
+    # extract this puppy out of here?
+    help_methods    = self.methods
+    help_methods    = help_methods.select {|name| name.start_with?(SimpleCLI.help_prefix) } if SimpleCLI.help_prefix
+    help_methods    = help_methods.select {|name| name.end_with?(SimpleCLI.help_suffix)   } if SimpleCLI.help_suffix
+
     command_methods = self.methods.select do |method_name|
-      help_methods.include? "#{ method_name }_help"
+      help_methods.include? "#{ SimpleCLI.help_prefix }#{ method_name }#{ SimpleCLI.help_suffix }"
     end
   end
 
--- simplecli3/spec/commands_spec.rb
+++ simplecli3/spec/commands_spec.rb
@@ -11,8 +11,15 @@ class ExampleClass
   end
 
   def regular_method *args
-    "called regular_method with #{ args.inspect }"
   end
+  
+  # uses custom prefix
+  def custom1; end
+  def documentation_for_custom1; end
+
+  # used custom prefix and suffix
+  def custom2; end
+  def documentation_for_custom2_stuff; end
 end
 
 describe SimpleCLI, 'commands' do
@@ -20,10 +27,30 @@ describe SimpleCLI, 'commands' do
   it 'should be able to find #command_names (on an instance)' do
     ExampleClass.new.command_names.should     include('command_method')
     ExampleClass.new.command_names.should_not include('regular_method')
+    ExampleClass.new.command_names.should_not include('custom1')
+    ExampleClass.new.command_names.should_not include('custom2')
   end
 
   it 'should be able to find #command_names (on a class ... cause, why not?)'
 
-  it 'should be able to change the prefix/suffix for defining the [command]_help method'
+  it 'should be able to change the prefix/suffix for defining the [command]_help method' do
+    default_prefix, default_suffix = SimpleCLI.help_prefix, SimpleCLI.help_suffix
+
+    SimpleCLI.help_prefix = 'documentation_for_'
+    SimpleCLI.help_suffix = nil
+
+    ExampleClass.new.command_names.should     include('custom1')
+    ExampleClass.new.command_names.should_not include('command_method')
+    ExampleClass.new.command_names.should_not include('regular_method')
+    ExampleClass.new.command_names.should_not include('custom2')
+
+    SimpleCLI.help_suffix = '_stuff'
+    ExampleClass.new.command_names.should     include('custom2')
+    ExampleClass.new.command_names.should_not include('command_method')
+    ExampleClass.new.command_names.should_not include('regular_method')
+    ExampleClass.new.command_names.should_not include('custom1')
+
+    SimpleCLI.help_prefix, SimpleCLI.help_suffix = default_prefix, default_suffix
+  end
 
 end
